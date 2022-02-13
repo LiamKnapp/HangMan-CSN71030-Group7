35be4caf7392e4579e809697d6633215a2a83732
Liam Knapp <Lknapp1110@conestogac.on.ca>
2022-02-13 17:07:13 -0500

    commented it out

--- HangMan - Teambasedsoftdev Group7/Main.c
+++ HangMan - Teambasedsoftdev Group7/Main.c
@@ -10,14 +10,14 @@ int main() {
 
 	/* Liam: */	
 	/* THIS IS THE RANDOM WORD THE USER WILL BE TRYING TO GUESS */
-	char *wordtoguess = malloc(sizeof (char*));
+	//char *wordtoguess = malloc(sizeof (char*));
 
 	/* THIS IS ALL THE FUNCTIONS FOR FILE STUFF */
-	FileDecrypt();//decrypt the word file
-	wordtoguess = FileGetWord(); // get random word
-	printf("Random word: %s\n", wordtoguess);
-	FileRemoveWord(wordtoguess); // remove word
-	FileEncrypt(); // encrypt the word file
+	//FileDecrypt();//decrypt the word file
+	//wordtoguess = FileGetWord(); // get random word
+	//printf("Random word: %s\n", wordtoguess);
+	//FileRemoveWord(wordtoguess); // remove word
+	//FileEncrypt(); // encrypt the word file
 
 
 	//// Minh: Create the trees for the program to store the data and sort it from highest to lowest
