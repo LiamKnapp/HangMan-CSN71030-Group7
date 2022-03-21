d8217dcb4be990304a71a1e73f8f3c75fe3c0cea
Le Minh Nguyen <Lnguyen3388@conestogac.on.ca>
2022-03-21 15:28:00 -0400

    Change the print hangman figure ( one argument ( int mistakes )

--- HangMan - Teambasedsoftdev Group7/UI.c
+++ HangMan - Teambasedsoftdev Group7/UI.c
@@ -220,38 +220,143 @@ mark:
 
 }
 
-void printBody(int mistakes) { // (The number of the mistakes , and an empty dynamic (malloc) array to store the address of each character compare with each number of mistakes )
-	printf("Mistakes :%d\n", mistakes);
-	char* body = malloc(sizeof(char*));
-
-	switch (mistakes) {
-
-	case 6: body[6] = '\\'; break;
-	case 5: body[5] = '/'; break;
-	case 4: body[4] = '\\'; break;
-	case 3: body[3] = '|'; break;
-	case 2: body[2] = '/'; break;
-	case 1: body[1] = ')', body[0] = '('; break;
-	default: break;
-
-	}
-
-	printf("\t _________\n"
-		"\t|         |\n"
-		"\t|        %c %c\n"
-		"\t|        %c%c%c\n"
-		"\t|        %c %c\n"
-		"\t|             \n"
-		"\t|             ", body[0], body[1], body[2],
-		body[3], body[4], body[5], body[6]);
-}
-
 
 void printWord(char* guess, int len) {
 	printf("\t");
 	for (int i = 0; i < len; ++i)
 	{
-	printf("%c ", guess[i]);
+		printf("%c ", guess[i]);
 	}
 	printf("\n\n");
 }
+
+void printBody(int mistakes)
+{
+
+	char* platform[] = {
+
+		"_____________\n",
+
+		"_____________\n"
+		"	   |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"   |     |   \n"
+		"   |     |   \n"
+		"    -----	  \n",
+
+		"_____________\n"
+		"	   |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"   |     |   \n"
+		"   |     |   \n"
+		"    -----	  \n"
+		"	   |      \n"
+		"      |      \n"
+		"      |      \n"
+		"      |      \n"
+		"      |      \n",
+
+		"______________\n"
+		"	   |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"	|     |   \n"
+		"   |     |   \n"
+		"    -----	  \n"
+		"	   |      \n"
+		"     /|      \n"
+		"    / |      \n"
+		"   /  |      \n"
+		"      |      \n"
+		"             \n"
+		"             \n"
+		"             \n"
+		"             \n",
+
+		"_____________\n"
+		"	   |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"   |     |   \n"
+		"   |     |   \n"
+		"    -----	  \n"
+		"	   |      \n"
+		"     /|\     \n"
+		"    / | \    \n"
+		"   /  |  \   \n"
+		"      |      \n"
+		"             \n"
+		"             \n"
+		"             \n"
+		"             \n",
+
+		"_____________\n"
+		"      |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"   |     |   \n"
+		"   |     |   \n"
+		"    -----	  \n"
+		"	   |      \n"
+		"     /|\     \n"
+		"    / | \    \n"
+		"   /  |  \   \n"
+		"      |      \n"
+		"     /       \n"
+		"    /        \n"
+		"   /         \n"
+		"             \n",
+
+		"_____________\n"
+		"	   |   	  \n"
+		"      |      \n"
+		"      |      \n"
+		"    _____    \n"
+		"   |     |   \n"
+		"   |     |   \n"
+		"    -----	  \n"
+		"	   |      \n"
+		"     /|\     \n"
+		"    / | \	  \n"
+		"   /  |  \   \n"
+		"      |      \n"
+		"     / \     \n"
+		"    /   \    \n"
+		"   /     \   \n"
+		"             \n"
+	};
+
+	switch (mistakes)
+	{
+	case 6:
+		printf("\n\n%s\n", platform[6]);
+		break;
+	case 5:
+		printf("\n\n%s\n", platform[5]);
+		break;
+	case 4:
+		printf("\n\n%s\n", platform[4]);
+		break;
+	case 3:
+		printf("\n\n%s\n", platform[3]);
+		break;
+	case 2:
+		printf("\n\n%s\n", platform[2]);
+		break;
+	case 1:
+		printf("\n\n%s\n", platform[1]);
+		break;
+	case 0:
+		printf("\n\n%s\n", platform[0]);
+		break;
+	}
+}
+
+
