221dc8ccd2b9badc2b5df1a4413107853528c613
William Kubica <Kirbycopy56@hotmail.com>
2022-03-21 17:17:34 -0400

    fixed MathToGuess

--- HangMan - Teambasedsoftdev Group7/MathToGuess.c
+++ HangMan - Teambasedsoftdev Group7/MathToGuess.c
@@ -3,26 +3,27 @@
 //HangMan
 #include "Header.h"
 
-int MathtoGuess(int lang) {
-	int oper = rand() % 4;
-	int num1 = rand() % 100;
-	int num2 = rand() % 100;
-	int ans;
+void MathtoGuess(int lang) {
+	int oper = rand() % 3;
+	int leftnum = rand() % 100;
+	int midnum = rand() % 100;
+	int rightnum;
 	int pos = rand() % 2;
 	char displayOper;
+	int correct;
 
 	switch (oper) {
 	case 0:
 		displayOper = '+';
-		ans = num1 + num2;
+		rightnum = leftnum + midnum;
 		break;
 	case 1:
 		displayOper = '-';
-		ans = num1 - num2;
+		rightnum = leftnum - midnum;
 		break;
 	case 2:
 		displayOper = 'x';
-		ans = num1 * num2;
+		rightnum = leftnum * midnum;
 		break;
 	default:
 		if (lang == 1) { // for english
@@ -33,41 +34,48 @@ int MathtoGuess(int lang) {
 		}
 		break;
 	}
-	
+	if (pos == 0) {
+		correct = leftnum;
+	} else {
+		correct = midnum;
+	}
 	for (int i = 0; i < 6; i++) {
+		printBody(i);
+		printEquation(pos, displayOper, leftnum, midnum, rightnum);
 		int guess = getGuess(lang);
-		if (!checkGuess(guess, lang)) {
-			printEquation(pos, oper, num1, num2, ans);
-			printBody(i);
-		}
-		else {
+		if (checkGuess(guess, correct, lang)) {
 			return;
 		}
 	}
+	if (lang == 1) {
+		printf("GAME OVER!!!\n");
+		printf("Correct answer was: %d\n", correct);
+	}
+	else if (lang == 2) {
 
-
+	}
 }
 
 int getGuess(int lang) {
 	int ans;
 	while (1) {
 		if (lang == 1) { // for english
-			printf("Please enter a number between 1 and 100\n");
+			printf("Please enter your answer (as an integer)\n");
 		}
 		if (lang == 2) { // for french
 			printf("Veuillez entrer un nombre entre 1 et 100\n");
 		}
 
 		if (scanf_s("%d", &ans)) {
-			if (ans > 0 && ans < 101) {
+			if (ans > -10000 && ans < 10000) {
 				return ans;
 			}
 			else {
 				if (lang == 1) { // for english
-					printf("number must be between 1 and 100\n");
+					printf("number must be between -10000 and 10000\n");
 				}
 				if (lang == 2) { // for french
-					printf("le nombre doit etre compris entre 1 et 100\n");
+					printf("le nombre doit etre compris entre -10000 et 10000\n");
 				}
 			}
 		}
@@ -79,37 +87,33 @@ int getGuess(int lang) {
 				printf("le nombre doit etre un entier\n");
 			}
 		}
-		fgets("");
+		char c[2];
+		fgets(c, 2, stdin);
 		return ans;
 	}
 }
 
-int checkGuess(int correct, int lang) {
-	int guess = getGuess(lang);
-	if (lang == 1) { // for english
-		if (guess == correct) {
+int checkGuess(int guess, int correct, int lang) {
+	
+	if (guess == correct) {
+		if(lang == 1) // for english
 			printf("Correct!!!\n");
-			return 1;
-		}
-		else if (guess < correct) {
-			printf("Too low!\n");
-		}
-		else if (guess > correct) {
-			printf("Too high!\n");
-		}
-	}
-
-	if (lang == 2) { // for french
-		if (guess == correct) { 
+		else if (lang == 2) // for french
 			printf("Bonne reponse!!!\n");
-			return 1;
-		}
-		else if (guess < correct) {
+
+		return 1;
+	}
+	else if (guess < correct) {
+		if (lang == 1) // for english
+			printf("Too low!\n");
+		else if (lang == 2) // for french
 			printf("Trop bas!\n");
-		}
-		else if (guess > correct) {
+	}
+	else if (guess > correct) {
+		if (lang == 1) // for english
+			printf("Too high!\n");
+		else if (lang == 2) // for french
 			printf("Trop haut!\n");
-		}
 	}
 	return 0;
 }
