b37fa7df4810041ca20f3f42a21223779416ff74
Le Minh Nguyen <Lnguyen3388@conestogac.on.ca>
2022-03-28 17:22:52 -0400

    Write the instruction how to set up the Native Test environment

--- HangmanTestCasesGroup7/HangmanTestCasesGroup7.cpp
+++ HangmanTestCasesGroup7/HangmanTestCasesGroup7.cpp
@@ -5,15 +5,42 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 extern "C" { // call functions 
 
-	// Liams Functions
-	int FileEncrypt(int);
-	int FileDecrypt(int);
-	char* FileGetWord(int);
-	int FileRemoveWord(char[], int);
-	int FileReuseWords(int);
-
-	
+	#include "Header.h"
 }
+
+// Step to set up your environment
+
+	//	Step 1: Go to the
+	//		- Configuration Properties > Linker > Input
+	//		- Select Additional Dependencies
+	//		- Use the dropdown arrow on the right to select Edit
+	//		- Add new line
+	//		- Copy this into that box correctly
+	//			File.obj
+	//			Main.obj
+	//			UI.obj
+	//			MathToGuess.obj
+	//			WordToGuess.obj
+	//		- Select Apply
+
+	//	Step 2: Go to the
+	//		- Configuration Properties > Linker > General
+	//		- Select Additional Library Directories
+	//		- Use the dropdown arrow on the right to select Edit
+	//		- Add new line
+	//		- Input to it the path to choose your Debug folder inside the Hangman - Teambasedsofdev Group7
+	//		Example on my computer is: C:\Users\Minh\Desktop\Study\Semester II - 2022\ASSIGNMENT\Project - Github - Big\HangMan - Teambasedsoftdev Group7\Debug
+	//		- Select Apply
+
+	//	Step 3: Go to the
+	//		- Configuration Properties > VC++
+	//		- Select Include Directories
+	//		- Use the dropdown arrow on the right to select Edit
+	//		- Add new line
+	//		- Input to it the path to store the Header.h file inside the HangMan - Teambasedsofdev Group7
+	//			Example on my computer is: C:\Users\Minh\Desktop\Study\Semester II - 2022\ASSIGNMENT\Project - Github - Big\HangMan - Teambasedsoftdev Group
+	//		- Select Apply
+
 
 namespace HangmanTestCasesGroup7
 {
