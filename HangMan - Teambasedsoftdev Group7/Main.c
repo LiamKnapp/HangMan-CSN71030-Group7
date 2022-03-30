0cf11c195f9f7a7fc770745b464cbfaa3bae8b3f
Liam Knapp <Lknapp1110@conestogac.on.ca>
2022-03-30 13:52:05 -0400

    removed comments

--- HangMan - Teambasedsoftdev Group7/Main.c
+++ HangMan - Teambasedsoftdev Group7/Main.c
@@ -44,31 +44,6 @@ mark:
 	}
 	
 	BSTInit(lang);
-	// ----------------------------------------------------------------------------------------------------------------------
-
-	/* Liam: */
-	/* Everything related to files: */
-
-	/* All file function names and description for quick access (do not uncomment):
-	
-	FileEncrypt(lang);                 // encrypts the file that gets specified by the language the user selected
-	FileDecrypt(lang);                 // decrypts the file that gets specified by the language the user selected
-	FileGetWord(lang);                 // calls the function that randomly selects a word from the file specified by the users language
-	FileRemoveWord(wordtoguess, lang); // removes the word the user guessed from the file and stores it into the save word file
-	FileReuseWords(lang);              // amends the save words file with the words file to reuse all the words previously guessed
-	
-	*/
-
-	// ----------------------------------------------------------------------------------------------------------------------
-
-	/* Minh: */
-	/* Create the trees for the program to store the data and sort it from highest to lowest: */
-	
-	//BSTInit();								//Initialize the BST
-	//GameInfor* p = NULL;						//Define node p as null
-	//p = (GameInfor*)malloc(sizeof(GameInfor));
-
-	// ----------------------------------------------------------------------------------------------------------------------
 
 	return 0;
 }
